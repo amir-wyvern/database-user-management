04eca8238ef67efafd914fe48bfb7c263448e172
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-01-03 06:02:00 -0500

    feat: add NewUser rpc to DataBaseService

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from concurrent import futures
 import grpc_utils.database_pb2_grpc as pb2_grpc
 import grpc_utils.database_pb2 as pb2
 import logging
-import os
 from db.inital import init_database
+from db.database import get_db
+from schemas import UserRegisterForDataBase
+import os
+
+from db import db_user
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -39,27 +43,48 @@ else:
 class DataBaseService(pb2_grpc.DataBaseServicer):
 
      def NewUser(self, request, context):
-          return pb2.BaseResponse(**{'message': 'success' , 'code': 1001})
+
+        receive_data = {
+            'name': request.name,
+            'username': request.username,
+            'password': request.password,
+            'phone_number': request.phone_number,
+            'email': request.email,
+            'role': request.role
+        }
+        try:
+        
+            db_user.create_user(UserRegisterForDataBase(**receive_data) ,get_db().__next__())
+            logging.info(f'create user was successfully [username: {request.username}]')
+
+        except Exception as e:
+        
+            logging.error(f'error [fn: NewUser err_msg: {e}')
+
+            return pb2.BaseResponse(**{'message': 'failed' , 'code': 1400})
+
+
+        return pb2.BaseResponse(**{'message': 'success' , 'code': 1200})
 
 
 def serve():
 
-     try:
+    try:
 
-          init_database(ADMIN_USERNAME, ADMIN_PASSWORD)
+        init_database(ADMIN_USERNAME, ADMIN_PASSWORD)
 
-          logger.info(f'service grpc database is runing [::]:{PORT} ...')
+        logger.info(f'service grpc database is runing [::]:{PORT} ...')
 
-          server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-          pb2_grpc.add_DataBaseServicer_to_server(DataBaseService(), server)
-          server.add_insecure_port(f'[::]:{PORT}')
-          server.start()
-          server.wait_for_termination()
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        pb2_grpc.add_DataBaseServicer_to_server(DataBaseService(), server)
+        server.add_insecure_port(f'[::]:{PORT}')
+        server.start()
+        server.wait_for_termination()
 
-     except Exception as e:
-          
-          logger.error(f'occure error: [{e}]')
-          exit(0)
+    except Exception as e:
+        
+        logger.error(f'occure error: [{e}]')
+        exit(0)
          
 serve()
 
