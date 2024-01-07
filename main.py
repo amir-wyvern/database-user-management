d10cdbe88eb0a8aeec1adcbe98098371d236cfc4
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-01-07 16:42:28 -0500

    fix: fix bug in use get_db object & add more check for arguments
    - In this commit, unlike before, we use an object to request the database
    - Add rollback for remove uncommit updates in per requests (ModifyUserInfo)
    - Add password hashing to ModifyUserPassword method
    - Add exists check for NewUser Method (email, phone_number)

--- main.py
+++ main.py
@@ -56,9 +56,9 @@ class DataBaseService(pb2_grpc.DataBaseServicer):
      def GetUser(self, request, context):
 
         logging.debug(f'[GetUser] Receive a Requests [username: {request.username}]')
-
+        db = get_db().__next__()
         try:
-            user = db_user.get_user_by_username(request.username ,get_db().__next__())
+            user = db_user.get_user_by_username(request.username, db)
             print()
             if user is None:
                 logging.debug(f'[GetUser] Username Not Found [username: {request.username}]')
@@ -85,25 +85,36 @@ class DataBaseService(pb2_grpc.DataBaseServicer):
      def NewUser(self, request, context):
     
         logging.debug(f'[NewUser] Receive a Requests [username: {request.username}]')
-    
+        db = get_db().__next__()
+
         try:
             
-            user = db_user.get_user_by_username(request.username ,get_db().__next__())
+            user = db_user.get_user_by_username(request.username, db)
 
             if user:
                 logging.debug(f'[NewUser] Username already exists [username: {request.username}]')
                 return pb2.BaseResponse(**{'message': 'Username already exist' , 'code': 1403})
             
+            if request.email and db_user.get_user_by_email(request.email, db):
+                logging.debug(f'[NewUser] Email already exists [username: {request.username} -email: {request.email}]')
+                return pb2.BaseResponse(**{'message': 'Email already exist' , 'code': 1406})
+            
+            if db_user.get_user_by_phone_number(request.phone_number, db):
+                logging.debug(f'[NewUser] PhoneNumber already exists [username: {request.username} -phone_number: {request.phone_number}]')
+                return pb2.BaseResponse(**{'message': 'PhoneNumber already exists' , 'code': 1407})
+
             receive_data = {
                 'name': request.name,
                 'username': request.username,
                 'password': pwd_context.hash(request.password),
                 'phone_number': request.phone_number,
-                'email': request.email,
                 'role': map_enums[request.role]
             }
 
-            db_user.create_user(UserRegisterForDataBase(**receive_data) ,get_db().__next__())
+            if request.email:
+                receive_data.update({'email': request.email})
+
+            db_user.create_user(UserRegisterForDataBase(**receive_data) ,db)
             logging.info(f'[NewUser] create user was successfully [username: {request.username}]')
 
             return pb2.BaseResponse(**{'message': 'success' , 'code': 1200})
@@ -117,15 +128,16 @@ class DataBaseService(pb2_grpc.DataBaseServicer):
      def ModifyUserPassword(self, request, context):
 
         logging.debug(f'[ModifyUserPassword] Receive a Requests [username: {request.username}]')
+        db = get_db().__next__()
 
         try:
-            user = db_user.get_user_by_username(request.username ,get_db().__next__())
+            user = db_user.get_user_by_username(request.username, db)
 
             if user is None:
                 logging.debug(f'[ModifyUserPassword] Username Not Found [username: {request.username}]')
                 return pb2.BaseResponse(**{'message': 'Username Not Found' ,'code': 1401})
             
-            db_user.update_password(user.user_id, request.password ,get_db().__next__())
+            db_user.update_password(user.user_id, pwd_context.hash(request.password), db)
             logging.info(f'[ModifyUserPassword] user password updated successfully [username: {request.username}]')
 
             return pb2.BaseResponse(**{'message': 'success' ,'code': 1200})
@@ -139,9 +151,10 @@ class DataBaseService(pb2_grpc.DataBaseServicer):
      def ModifyUserInfo(self, request, context):
 
         logging.debug(f'[ModifyUserInfo] Receive a Requests [username: {request.username}]')
+        db = get_db().__next__()
 
         try:
-            user = db_user.get_user_by_username(request.username ,get_db().__next__())
+            user = db_user.get_user_by_username(request.username, db)
 
             if user is None:
                 logging.debug(f'[ModifyUserInfo] Username Not Found [username: {request.username}]')
@@ -152,16 +165,27 @@ class DataBaseService(pb2_grpc.DataBaseServicer):
                 return pb2.BaseResponse(**{'message': 'There is no field to update' ,'code': 1402})
 
             if request.name:
-                db_user.update_name(user.user_id, request.password ,get_db().__next__(), commit= False)
-            
+                db_user.update_name(user.user_id, request.name, db, commit= False)
+
             if request.email:
-                db_user.update_email(user.user_id, request.email ,get_db().__next__(), commit= False)
+                if db_user.get_user_by_email(request.email, db):
+                    
+                    db.rollback()
+                    logging.debug(f'[ModifyUserInfo] Email already exists [username: {request.username} -email: {request.email}]')
+                    return pb2.BaseResponse(**{'message': 'Email already exist' , 'code': 1406})
+                
+                db_user.update_email(user.user_id, request.email ,db, commit= False)
             
             if request.phone_number:
-                db_user.update_phone_number(user.user_id, request.phone_number ,get_db().__next__(), commit= False)
+                if db_user.get_user_by_phone_number(request.phone_number, db):
+                    
+                    db.rollback()
+                    logging.debug(f'[ModifyUserInfo] PhoneNumber already exists [username: {request.username} -phone_number: {request.phone_number}]')
+                    return pb2.BaseResponse(**{'message': 'PhoneNumber already exist' , 'code': 1407})
+                
+                db_user.update_phone_number(user.user_id, request.phone_number, db, commit= False)
 
-            get_db().__next__().commit()
-
+            db.commit()
             logging.info(f'[ModifyUserInfo] Username info updated successfully [username: {request.username}]')
 
             return pb2.BaseResponse(**{'message': 'success' ,'code': 1200})
@@ -175,15 +199,16 @@ class DataBaseService(pb2_grpc.DataBaseServicer):
      def ModifyUserRole(self, request, context):
 
         logging.debug(f'[ModifyUserRole] Receive a Requests [username: {request.username}]')
+        db = get_db().__next__()
 
         try:
-            user = db_user.get_user_by_username(request.username ,get_db().__next__())
+            user = db_user.get_user_by_username(request.username, db)
 
             if user is None:
                 logging.debug(f'[ModifyUserRole] Username Not Found [username: {request.username}]')
                 return pb2.BaseResponse(**{'message': 'Username Not Found' ,'code': 1401})
             
-            db_user.update_role(user.user_id ,request.role ,get_db().__next__())
+            db_user.update_role(user.user_id, map_enums[request.role], db)
             logging.info(f'[ModifyUserRole] Username role updated successfully [username: {request.username} -new_role: {request.role}]')
 
             return pb2.BaseResponse(**{'message': 'success' ,'code': 1200})
@@ -197,15 +222,16 @@ class DataBaseService(pb2_grpc.DataBaseServicer):
      def DeleteUser(self, request, context):
 
         logging.debug(f'[DeleteUser] Receive a Requests [username: {request.username}]')
+        db = get_db().__next__()
 
         try:
-            user = db_user.get_user_by_username(request.username ,get_db().__next__())
+            user = db_user.get_user_by_username(request.username, db)
 
             if user is None:
                 logging.debug(f'[DeleteUser] Username Not Found [username: {request.username}]')
                 return pb2.BaseResponse(**{'message': 'Username Not Found' ,'code': 1401})
             
-            db_user.delete_user(request.phone_number ,get_db().__next__())
+            db_user.delete_user(user.user_id, db)
             logging.debug(f'[DeleteUser] Username deleted successfully [username: {request.username}]')
 
             return pb2.BaseResponse(**{'message': 'success' ,'code': 1200})
