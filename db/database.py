ac3d20a97373838b045d4ffa23e420099239be47
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-01-07 14:28:00 -0500

    fix: add inspect function to database.py file for access this item from outside the file

--- db/database.py
+++ db/database.py
@@ -1,19 +1,18 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
 import os
 
 POSTGRES_USERNAME = os.getenv('POSTGRES_USERNAME')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_DATABASE = os.getenv('POSTGRES_DATABASE')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-POSTGRES_DATABASE = os.getenv('POSTGRES_DATABASE')
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-Base = declarative_base() 
+Base = declarative_base()
 
 sessionlocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
