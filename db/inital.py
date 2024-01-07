a040847c07dd6cb5e595cf887796de68f722acc6
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-01-07 14:30:16 -0500

    fix: remove logging part for prevent double logging & add CryptContext for hash password

--- db/inital.py
+++ db/inital.py
@@ -1,36 +1,21 @@
-import logging
-from db import models
-from db.database import get_db, engine
-from db.db_user import create_user
 from schemas import UserRegisterForDataBase, UserRole
+from db.database import get_db, engine, inspect
+from passlib.context import CryptContext
+from db.db_user import create_user
+from db import models
+import logging 
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# Create a console handler to show logs on terminal
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s | %(message)s')
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
+def init_database(admin_username: str, admin_password: str, logger: logging):
 
-# Create a file handler to save logs to a file
-file_handler = logging.FileHandler('database_api.log')
-file_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s | %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-
-def init_database(admin_username, admin_password):
-
-     if not engine.dialect.has_table(engine, "user"):
+     if not inspect(engine).has_table("user"):
           models.Base.metadata.create_all(engine)
           logger.info('DataBase Created')
           
           user_admin = UserRegisterForDataBase(
                username= admin_username,
-               password= admin_password,
+               password= pwd_context.hash(admin_password),
                phone_number= "+98-0000000000",
                name= "Admin",
                role= UserRole.ADMIN
